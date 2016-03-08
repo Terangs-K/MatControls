521ff9c3313719e14913e8c74616cb435f7d630b
TERANG <jopd119@naver.com>
2016-03-08 17:52:44 +0900

    Home Activity Commit

---
+++ app/src/main/java/com/example/c07656/matcontrols/HomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.c07656.matcontrols;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Log.d("MatHomeActivity","Init Home Activity!!");
+    }
+}
--- app/src/main/java/com/example/c07656/matcontrols/MainActivity.java
+++ app/src/main/java/com/example/c07656/matcontrols/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.c07656.matcontrols;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.projectlist, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                MainActivity.this.startActivity(i);
+            }
+        });
     }
 }
