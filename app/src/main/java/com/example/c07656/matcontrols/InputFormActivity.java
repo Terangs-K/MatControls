e92feca1fa52b2a0a366d01e4459f86fbea82971
TERANG <jopd119@naver.com>
2016-03-16 18:07:44 +0900

    Multi Select Image Commit

--- app/src/main/java/com/example/c07656/matcontrols/InputFormActivity.java
+++ app/src/main/java/com/example/c07656/matcontrols/InputFormActivity.java
@@ -1,8 +1,11 @@
 package com.example.c07656.matcontrols;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -11,14 +14,22 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+import me.nereo.multi_image_selector.MultiImageSelectorActivity;
+import me.nereo.multi_image_selector.utils.FileUtils;
 
 public class InputFormActivity extends Activity {
 
     ArrayAdapter adapter;
+    ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9, img10;
+
     private int TAKE_CAMERA = 1;
     private int TAKE_GALLERY = 2;
-    EditText editPath;
+    private ArrayList<String> mSelectPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +37,20 @@ public class InputFormActivity extends Activity {
         setContentView(R.layout.activity_inputform);
         Log.d("MatInputFormActivity", "Init InputForm Activity!!");
 
-        editPath = (EditText)findViewById(R.id.editImgList);
-
         Spinner spinner01 = (Spinner) findViewById(R.id.listPunchGroup);
         Spinner spinner02 = (Spinner) findViewById(R.id.listDiscipline);
         Spinner spinner03 = (Spinner) findViewById(R.id.listPriority);
+
+        img1 = (ImageView) findViewById(R.id.gridImage01);
+        img2 = (ImageView) findViewById(R.id.gridImage02);
+        img3 = (ImageView) findViewById(R.id.gridImage03);
+        img4 = (ImageView) findViewById(R.id.gridImage04);
+        img5 = (ImageView) findViewById(R.id.gridImage05);
+        img6 = (ImageView) findViewById(R.id.gridImage06);
+        img7 = (ImageView) findViewById(R.id.gridImage07);
+        img8 = (ImageView) findViewById(R.id.gridImage08);
+        img9 = (ImageView) findViewById(R.id.gridImage09);
+        img10 = (ImageView) findViewById(R.id.gridImage10);
 
         adapter = ArrayAdapter.createFromResource(this, R.array.punchgrouplist, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -59,9 +79,16 @@ public class InputFormActivity extends Activity {
         btnSaveGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_PICK);
-                i.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-                i.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                Intent i = new Intent(InputFormActivity.this, MultiImageSelectorActivity.class);
+                i.putExtra(MultiImageSelectorActivity.EXTRA_SHOW_CAMERA, false);
+                i.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_COUNT, 10);
+                i.putExtra(MultiImageSelectorActivity.EXTRA_SELECT_MODE, MultiImageSelectorActivity.MODE_MULTI);
+
+                if(mSelectPath != null && mSelectPath.size()>0) {
+                    i.putExtra(MultiImageSelectorActivity.EXTRA_DEFAULT_SELECTED_LIST, mSelectPath);
+                }
+
                 startActivityForResult(i, TAKE_GALLERY);
             }
         });
@@ -72,10 +99,95 @@ public class InputFormActivity extends Activity {
         if(resultCode == RESULT_OK){
             if(requestCode == TAKE_CAMERA){
                 Uri currImageURI = data.getData();
-                editPath.setText("CAMERA : " + getRealPathFromURI(currImageURI));
-            }else if(requestCode == TAKE_GALLERY){
-                Uri currImageURI = data.getData();
-                editPath.setText("GALLERY : " + getRealPathFromURI(currImageURI));
+                img1.setImageURI(currImageURI);
+            } else if (requestCode == TAKE_GALLERY){
+
+                mSelectPath = data.getStringArrayListExtra(MultiImageSelectorActivity.EXTRA_RESULT);
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < 10; i++) {
+
+                    try {
+                        if (i >= mSelectPath.size()) {
+                            switch (i) {
+                                case 0:
+                                    img1.setImageDrawable(getResources().getDrawable(R.drawable.asv));
+                                    break;
+                                case 1:
+                                    img2.setVisibility(View.GONE);
+                                    break;
+                                case 2:
+                                    img3.setVisibility(View.GONE);
+                                    break;
+                                case 3:
+                                    img4.setVisibility(View.GONE);
+                                    break;
+                                case 4:
+                                    img5.setVisibility(View.GONE);
+                                    break;
+                                case 5:
+                                    img6.setVisibility(View.GONE);
+                                    break;
+                                case 6:
+                                    img7.setVisibility(View.GONE);
+                                    break;
+                                case 7:
+                                    img8.setVisibility(View.GONE);
+                                    break;
+                                case 8:
+                                    img9.setVisibility(View.GONE);
+                                    break;
+                                case 9:
+                                    img10.setVisibility(View.GONE);
+                                    break;
+                            }
+                        } else {
+                            switch (i) {
+                                case 0:
+                                    img1.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 1:
+                                    img2.setVisibility(View.VISIBLE);
+                                    img2.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 2:
+                                    img3.setVisibility(View.VISIBLE);
+                                    img3.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 3:
+                                    img4.setVisibility(View.VISIBLE);
+                                    img4.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 4:
+                                    img5.setVisibility(View.VISIBLE);
+                                    img5.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 5:
+                                    img6.setVisibility(View.VISIBLE);
+                                    img6.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 6:
+                                    img7.setVisibility(View.VISIBLE);
+                                    img7.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 7:
+                                    img8.setVisibility(View.VISIBLE);
+                                    img8.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 8:
+                                    img9.setVisibility(View.VISIBLE);
+                                    img9.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                                case 9:
+                                    img10.setVisibility(View.VISIBLE);
+                                    img10.setImageBitmap(loadFilePathtoBitmap(mSelectPath.get(i)));
+                                    break;
+                            }
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         }
     }
@@ -87,5 +199,13 @@ public class InputFormActivity extends Activity {
         cursor.moveToFirst();
         return cursor.getString(column_index);
 
+    }
+
+    public static Bitmap loadFilePathtoBitmap(String filePath) throws Exception, OutOfMemoryError {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inSampleSize = 6;
+        Bitmap retunrBit = BitmapFactory.decodeFile(filePath, opt);
+
+        return retunrBit;
     }
 }
